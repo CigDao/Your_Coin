a7ab4befa78cb3c6c6b35d902885e9e6df696be1
Jonathan Green <dev.jonathan.green@gmail.com>
2023-10-29 11:44:59 -0400

    WIP

--- reinstall.sh
+++ reinstall.sh
@@ -16,23 +16,6 @@ export PRE_MINTED_TOKENS=10_000_000_000_005_000_000
 export MAX_SUPPLY=100_000_000_000_000_000_000
 export TRANSFER_FEE=1_000_000
 
-#dfx deploy --network ic icrc1_ledger_canister --argument "(variant {Init = 
-#record {
-#     token_symbol = \"${TOKEN_SYMBOL}\";
-#     token_name = \"${TOKEN_NAME}\";
-#     minting_account = record { owner = principal \"${MINTER}\" };
-#     transfer_fee = ${TRANSFER_FEE};
-#     metadata = vec {};
-#     initial_balances = vec { record { record { owner = principal \"${SONIC}\"; }; ${PRE_MINTED_TOKENS}; }; };
-#     archive_options = record {
-#         num_blocks_to_archive = ${NUM_OF_BLOCK_TO_ARCHIVE};
-#         trigger_threshold = ${TRIGGER_THRESHOLD};
-#         controller_id = principal \"${ARCHIVE_CONTROLLER}\";
-#         cycles_for_archive_creation = opt ${CYCLE_FOR_ARCHIVE_CREATION};
-#     };
-# }
-#})"
-
 dfx canister --network ic install icrc1_ledger_canister --argument "(variant {Init = 
 record {
      token_symbol = \"${TOKEN_SYMBOL}\";
